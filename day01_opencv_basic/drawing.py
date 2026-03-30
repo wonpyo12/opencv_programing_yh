5455ec9f2925c9f63f73ff722bd22748ff63efa8
wonpyo12 <wonpyo0719@naver.com>
2026-03-30 14:33:16 +0900

    도형 그리기실습

---
+++ day01_opencv_basic/drawing.py
@@ -0,0 +1,33 @@
+﻿import numpy as np
+import cv2 as cv
+ 
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+ 
+# Draw a diagonal blue line with thickness of 5 px
+cv.line(img,(0,0),(511,511),(255,0,0),5)
+
+cv.rectangle(img,(384,0),(510,128),(0,255,0),3)
+
+cv.circle(img,(447,63), 63, (0,0,255), -1)
+
+cv.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+
+cv.line(img,(511,0),(0,511),(0,255,255),2)
+
+cv.ellipse(img,(256,256),(150,80),0,0,360,(255,255,255),4)
+
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+cv.polylines(img,[pts],True,(0,255,255))
+
+pts = np.array([[250,130],[200,160],[300,160]], np.int32)
+pts = pts.reshape((-1,1,2))
+cv.polylines (img,[pts], True ,(255,0,0),5)
+
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.putText(img,'OpenCV',(10,500), font, 4,(255,255,255),2,cv.LINE_AA)
+
+cv.imshow("Drawing image",img)
+cv.waitKey(0)
+cv.destroyAllWindows
