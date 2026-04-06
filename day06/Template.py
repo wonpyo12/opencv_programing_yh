f3430a4ce1379cc713a7a0dd17e0210df3273e7b
wonpyo12 <wonpyo0719@naver.com>
2026-04-06 13:28:53 +0900

    실습1

---
+++ day06/Template.py
@@ -0,0 +1,43 @@
+﻿import urllib.request
+import os
+import cv2 as cv 
+from matplotlib import pyplot as plt 
+import numpy as np
+
+def get_sample(filename, repo='opencv'):
+    """부교 또는 OpenCV 공식 샘플 이미지 자동 다운로드
+    
+    Args:
+        filename (str): 이미지 파일명 (예: 'morphological.png')
+        repo (str): 'insightbook' (부교) 또는 'opencv' (공식)
+    
+    Returns:
+        str: 다운로드된 파일명
+    """
+    if not os.path.exists(filename):
+        if repo == 'insightbook':
+            url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
+        else:  # opencv 공식
+            url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    return filename
+
+# 사용 방법
+img = cv.imread(get_sample('morphological.png', repo='insightbook'))
+
+img_rgb = cv.imread('mario.png')
+#print(img_rgb)
+assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+template = cv.imread('coin.png', cv.IMREAD_GRAYSCALE)
+print(template)
+assert template is not None, "file could not be read, check with os.path.exists()"
+w, h = template.shape[::-1]
+
+res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
+cv.imwrite('res.png',img_rgb)
---
+++ day06/Template_Matching.py
@@ -0,0 +1,55 @@
+﻿import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import urllib.request
+import os
+
+def get_sample(filename, repo='insightbook'):
+    """부교 또는 OpenCV 공식 샘플 이미지 자동 다운로드
+    
+    Args:
+        filename (str): 이미지 파일명 (예: 'morphological.png')
+        repo (str): 'insightbook' (부교) 또는 'opencv' (공식)
+    
+    Returns:
+        str: 다운로드된 파일명
+    """
+    if not os.path.exists(filename):
+        if repo == 'insightbook':
+            url = f"https://raw.githubusercontent.com/dltpdn/insightbook.opencv_project_python/master/img/{filename}"
+        else:  # opencv 공식
+            url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    return filename
+
+# 이미지 로드
+img = cv.imread(get_sample('messi5.jpg'))
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# 템플릿 추출: 사이트에서 파일 받기
+template = cv.imread('template.jpg',cv.IMREAD_GRAYSCALE)
+
+# Template Matching — TM_CCOEFF_NORMED 방법
+result = cv.matchTemplate(gray, template, cv.TM_CCOEFF_NORMED)
+
+# 최적 매칭 위치 찾기
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+top_left = max_loc  # TM_CCOEFF_NORMED에서는 max_loc이 최적
+
+# 매칭 영역 크기 계산
+h, w = template.shape[:2]
+bottom_right = (top_left[0] + w, top_left[1] + h)
+
+# 결과 표시
+result_img = img.copy()
+cv.rectangle(result_img, top_left, bottom_right, (0, 255, 0), 2)
+
+# 화면에 표시
+cv.imshow('Template', cv.cvtColor(template, cv.COLOR_GRAY2BGR))
+cv.imshow('Result', result_img)
+cv.imshow('Result Map', result)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# 결과 저장
+cv.imwrite('template_matching_result.jpg', result_img)
